e955329d49580769f116cd08af0e72b3a572a2e7
Pedro Callil <pedrocallil@usp.br>
2021-04-14 23:49:55 -0300

    Added command line options to python solver and compilation instructions to README.

--- README.md
+++ README.md
@@ -6,8 +6,21 @@ para o programa em C utilizados no projeto final da disciplina.
 
 Os dois arquivos em python, `python_solver.py` e `plot.py` são,
 respectivamente, um script para resolver as equações diferenciais com o uso do
-método BDF (imprimindo em stdout os resultados) e um script para representar
+método BDF (imprimindo em `stdout` os resultados) e um script para representar
 graficamente os resultados obtidos com o uso do método das linhas.
+
+Vale notar que o script em python é a forma mais eficiente de se resolver o
+sistema de EDOs; como o programa em C foi executado algumas poucas vezes, para
+o caso reação-difusão, não se fez um esforço muito grande em sua otimização.
+
+Entretanto, caso exista algum interesse em se utilizar o programa, basta clonar
+esse repositório e compilar o programa:
+
+```
+git clone https://github.com/pedro-callil/OregonatorPQI3403
+cd OregonatorPQI3403
+make
+```
 
 Autores
 -------
--- python_solver.py
+++ python_solver.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 
 import sys
+import argparse
 from scipy.integrate import solve_ivp
 
 class data_struct ():
@@ -31,26 +32,47 @@ def differential (t, y, data):
     dZdt = 2*k3*A*X - kc*B*Z
     return [dXdt, dYdt, dZdt]
 
-def get_cmd_line_args (argv):
-    try:
-        data = data_struct(float(argv[1]), float(argv[2]), float(argv[3]),
-            float(argv[4]), float(argv[5]), float(argv[6]),
-            float(argv[7]), float(argv[8]))
-    except:
-        data = data_struct(1.28, 2.4e6, 33.6, 2400, 1, 1, 0.06, 0.02)
+def get_cmd_line_args (args):
+    data = data_struct(1.28, 2.4e6, 33.6, 2400, 1, 1, 0.06, 0.02)
+    if args.a is not None:
+        data.A = args.a
+    if args.b is not None:
+        data.B = args.b
+    if args.f is not None:
+        data.f = args.f
+    if args.i is not None:
+        data.k1 = args.i
+    if args.j is not None:
+        data.k2 = args.j
+    if args.k is not None:
+        data.k3 = args.k
+    if args.l is not None:
+        data.k4 = args.l
+    if args.m is not None:
+        data.kc = args.m
     return data
 
-def main ():
-    argv = sys.argv
-    data = get_cmd_line_args(argv)
+def main (args):
+    data = get_cmd_line_args(args)
     solution = solve_ivp( differential, (0,1000),
             [3e-6, 3e-6, 300e-6], method='BDF', args=[data],
             rtol=1e-6, atol=1e-12)
     t = solution.t
     y = solution.y
+    print('t,x,y,z')
     for i in range(len(t)):
-        print(str(t[i]) + "," + str(y[0][i]) + "," +
-                str(y[1][i]) + "," + str(y[2][i]))
+        print('{0},{1},{2},{3}'.format(t[i],y[0][i],y[1][i],y[2][i]))
 
-main()
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--a", type=float, help="<initial concentration of A>")
+    parser.add_argument("-b", "--b", type=float, help="<initial concentration of B>")
+    parser.add_argument("-f", "--f", type=float, help="<f>")
+    parser.add_argument("-i", "--i", type=float, help="<k1>")
+    parser.add_argument("-j", "--j", type=float, help="<k2>")
+    parser.add_argument("-k", "--k", type=float, help="<k3>")
+    parser.add_argument("-l", "--l", type=float, help="<k4>")
+    parser.add_argument("-m", "--m", type=float, help="<kc>")
+    args = parser.parse_args()
+    main(args)
 
